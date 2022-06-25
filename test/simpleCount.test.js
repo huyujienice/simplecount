6306b4aa4d6dc097b63500f593c436c47ebbb438
clarkhu <huyujienice@gmail.com>
2022-06-25 17:46:04 +0800

    :sparkles:

--- test/simpleCount.test.js
+++ test/simpleCount.test.js
@@ -7,12 +7,12 @@ let twoValue = `0.1`;
 let step = `1`;
 let continueTest = true;
 
-async function goontest() {
+async function testsimpletofixed() {
   if (!continueTest) return;
-  let result = BigNumber(oneValue).plus(twoValue);
-  let countreulst = add(oneValue, twoValue);
-  let str = `bignumberjs:adds ${oneValue} + ${twoValue} to equal ${result}`;
-  let nowstr = `simplecountjs:adds ${oneValue} + ${twoValue} to equal ${countreulst}`;
+  let result = BigNumber(oneValue).toFormat(3);
+  let countreulst = simpleToFixed(oneValue, 3);
+  let str = `bignumberjs:${oneValue}.toFormat(3)=${result}`;
+  let nowstr = `simplecount:${oneValue}.simpleToFixed(${oneValue},3)=${countreulst}`;
   if (result === countreulst) {
     console.log(str);
     console.log(nowstr);
@@ -24,13 +24,41 @@ async function goontest() {
     console.error(`count error :${str}`);
     console.error(`count error :${nowstr}`);
   }
-  twoValue = String(BigNumber(twoValue).plus(step));
-  if (twoValue > 100) {
+  oneValue = String(BigNumber(oneValue).plus(step));
+  if (oneValue > 100) {
     continueTest = false;
   }
   if (continueTest) {
-    goontest();
+    testsimpletofixed();
   }
 }
 
-goontest();
+testsimpletofixed();
+
+// async function goontestadd() {
+//   if (!continueTest) return;
+//   let result = BigNumber(oneValue).plus(twoValue);
+//   let countreulst = add(oneValue, twoValue);
+//   let str = `bignumberjs:adds ${oneValue} + ${twoValue} to equal ${result}`;
+//   let nowstr = `simplecountjs:adds ${oneValue} + ${twoValue} to equal ${countreulst}`;
+//   if (result === countreulst) {
+//     console.log(str);
+//     console.log(nowstr);
+//     continueTest = true;
+//   } else {
+//     // throw new Error(nowstr);
+//     console.error(Object.prototype.toString.call(str));
+//     console.error(Object.prototype.toString.call(nowstr));
+//     console.error(`count error :${str}`);
+//     console.error(`count error :${nowstr}`);
+//   }
+//   twoValue = String(BigNumber(twoValue).plus(step));
+//   if (twoValue > 100) {
+//     continueTest = false;
+//   }
+//   if (continueTest) {
+//     goontestadd();
+//   }
+// }
+
+// goontestadd();
