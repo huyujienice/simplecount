21af22bd4a51cd86f70bf92c7627aa9d6a518dbc
clarkhu <huyujienice@gmail.com>
2022-06-29 11:22:31 +0800

    :sparkles:

---
+++ src/buildindex.js
@@ -0,0 +1,6 @@
+const { add, sub, mul, divi, simpleToFixed } = require("./core/index");
+exports.add = add;
+exports.sub = sub;
+exports.mul = mul;
+exports.divi = divi;
+exports.simpleToFixed = simpleToFixed;
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,22 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  // entry: "./src/index.js",
+  entry: "./src/buildindex.js",
   mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: 'simplecount.js',
+    library: "simplecountjs"
   },
   devServer: {
     hot: true,
     open: true,
     port: 8099,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Hot Module Replacement",
-    }),
-  ],
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: "Hot Module Replacement",
+  //   }),
+  // ],
 };
