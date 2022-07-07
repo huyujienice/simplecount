a95d39a16f0d7212f1500728c8385077d47d1d95
clarkhu <huyujienice@gmail.com>
2022-07-07 16:42:38 +0800

    :sparkles:

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,22 @@
 declare module "simplecount" {
-  export function add(one: string | number, two: string | number): string;
-  export function sub(one: string | number, two: string | number): string;
-  export function mul(one: string | number, two: string | number): string;
-  export function divi(one: string | number, two: string | number): string;
-  export function simpleToFixed(num: string | number, len?: number): string;
+  export function add(
+    one: string | number | BigInt,
+    two: string | number | BigInt
+  ): string;
+  export function sub(
+    one: string | number | BigInt,
+    two: string | number | BigInt
+  ): string;
+  export function mul(
+    one: string | number | BigInt,
+    two: string | number | BigInt
+  ): string;
+  export function divi(
+    one: string | number | BigInt,
+    two: string | number | BigInt
+  ): string;
+  export function simpleToFixed(
+    num: string | number | BigInt,
+    len?: number
+  ): string;
 }
